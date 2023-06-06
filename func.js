38e3c96859d628472fc8caba216a0901eff372ba
rfbplayz <135411842+rfbplayz@users.noreply.github.com>
2023-06-06 12:53:03 +0600

    Update func.js

--- func.js
+++ func.js
@@ -32,6 +32,82 @@ else if(user == "Kabbo"){
     document.getElementById("my").innerHTML = "close friend";
 }
 
+else if(user == "Borsha"){
+    document.getElementById("my").innerHTML = "wife";
+}
+
+else if(user == "Ayrin"){
+    document.getElementById("my").innerHTML = "love";
+}
+
+else if(user == "Nusa"){
+    document.getElementById("my").innerHTML = "cousin";
+}
+
+else if(user == "Aifa"){
+    document.getElementById("my").innerHTML = "sister";
+}
+
+else if(user == "Redwan" || user == "Borshon"){
+    document.getElementById("my").innerHTML = "Heyyy!! thats my name!!";
+}
+
+else if(user == "Tanzim"){
+    document.getElementById("my").innerHTML = "online friend";
+}
+
+else if(user == "Niloy"){
+    document.getElementById("my").innerHTML = "bench mate";
+}
+
+else if(user == "Borno"){
+    document.getElementById("my").innerHTML = "brother";
+}
+
+else if(user == "Fardin"){
+    document.getElementById("my").innerHTML = "close friend";
+}
+
+else if(user == "Mahir"){
+    document.getElementById("my").innerHTML = "ewww he is gay";
+}
+
+else if(user == "Samin"){
+    document.getElementById("my").innerHTML = "nigga";
+}
+
+else if(user == "Nubaid" || user == "Noobaid"){
+    document.getElementById("my").innerHTML = "junior";
+}
+
+else if(user == "Joy" || user == "Bhoumik"){
+    document.getElementById("my").innerHTML = "BHOOOO!!!";
+}
+
+else if(user == "Rifat"){
+    document.getElementById("my").innerHTML = "close friend";
+}
+
+else if(user == "Tazwar"){
+    document.getElementById("my").innerHTML = "close friend";
+}
+
+else if(user == "Samir"){
+    document.getElementById("my").innerHTML = "Motka Baba";
+}
+
+else if(user == "Pronob" || user == "Pornob"){
+    document.getElementById("my").innerHTML = "nah bro..iam not making a lesbo my friend";
+}
+
+else if(user == "Rafid"){
+    document.getElementById("my").innerHTML = "Protibondi LMAO";
+}
+
+else if(user == "Kabbow"){
+    document.getElementById("my").innerHTML = "sibling";
+}
+
 else{
     document.getElementById("my").innerHTML = "idk you or maybe you spelled your name wrong";
 }
